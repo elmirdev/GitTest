862047319c86201916ae334cab5e8e2673a4ccce
elmirdev <elmir.ismayilzada@gmail.com>
2023-07-03 20:06:15 +0400

    first commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Elmir Ismayilzada on 10.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //view.backgroundColor = .red
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
